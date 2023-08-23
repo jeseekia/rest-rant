4f7f7ba2bb74252e11c8412e27020d5db3295fe9
Jeseekia Vaughn <jeseekia@gmail.com>
2023-08-22 21:22:27 -0400

    Add a places controller and stub for index page

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+app.use('/places', require('./controllers/places'))
+
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
